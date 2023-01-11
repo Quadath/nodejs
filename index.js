de971688a16bf2ccb0868bed33d760089c0f761f
Quadatz <quadath@gmail.com>
2023-01-11 12:21:30 +0300

    URL and path

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const path = require('path');
 
-console.log(path.dirname);
+// console.log(__dirname);
+// console.log(path.join(__dirname, '..'));
+
+// console.log("Абсолютный путь", path.resolve('first', 'second', 'third')); 
+// const fullpath = path.resolve('first', 'second', 'third');
+// console.log('Парсинг пути', path.parse(fullpath))
+
+//URL
+const siteURL = 'http://localhost:8888/users?id=5123'
+const url = new URL(siteURL);
+console.log(url);
