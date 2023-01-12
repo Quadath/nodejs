b66b89248723afcfcc27a0586fd010077d1c7acd
Quadatz <quadath@gmail.com>
2023-01-12 14:58:39 +0300

    user controller

---
+++ controller/UsersController.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const response = require('./../response')
+
+exports.users = (req, res) => {
+    const users = [
+        {
+            "id" : 1,
+            "name": "Alex"
+        },
+        {
+            "id": 2,
+            "name": "Jonh"
+        }
+    ]
+    response.status(users, res);
+}
--- settings/routes.js
+++ settings/routes.js
@@ -2,6 +2,8 @@
 
 module.exports = (app) => {
     const indexController = require('./../controller/IndexController')
+    const usersController = require('./../controller/UsersController')
 
     app.route('/').get(indexController.index)
+    app.route('/users').get(usersController.users)
 }
