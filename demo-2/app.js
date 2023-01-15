2de4667ca02cf4eae89ea18bf961d61ba7ddf22a
Quadatz <quadath@gmail.com>
2023-01-15 15:58:25 +0300

    demo-2

---
+++ demo-2/app.js
@@ -0,0 +1,13 @@
+// const { characters, stealRing } = require('./characters.js')
+const log = require('./characters.js')
+
+log();
+const a = 2;
+
+// let myChars = characters;
+
+// myChars = stealRing(characters, "Anton")
+
+// for (const c of characters) {
+//     console.log(c)
+// }
