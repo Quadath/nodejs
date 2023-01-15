8044657da68434d863aa457fdbcb180d1df3efc7
Quadatz <quadath@gmail.com>
2023-01-15 15:47:12 +0300

    fs

--- settings/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = (app) => {
-    const indexController = require('./../controller/IndexController')
-    const usersController = require('./../controller/UsersController')
-
-    app.route('/').get(indexController.index)
-    app.route('/users').get(usersController.users)
-}
