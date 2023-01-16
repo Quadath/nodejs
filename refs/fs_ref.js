d2ac7437149cb41488a3e86c918e0a2d7fb08771
Quadatz <quadath@gmail.com>
2023-01-16 17:11:15 +0300

    file system

---
+++ refs/fs_ref.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.mkdir(path.join(__dirname, 'notes'), err => {
+    if (err) throw err
+    console.log('Папка создана')
+})
---
+++ user.js
@@ -0,0 +1,6 @@
+const user = {
+    name: 'Elena',
+    age: 26
+}
+
+module.exports = user
