962dba17a374a7b9425263dc32234ce506f4b7b3
Quadatz <quadath@gmail.com>
2023-01-16 17:26:45 +0300

    file system read

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.mkdir(path.join(__dirname, 'notes'), err => {
+// fs.mkdir(path.join(__dirname, 'notes'), err => {
+//     if (err) throw err
+//     console.log('Папка создана')
+// })
+
+fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'), 'Hello world', err => {
     if (err) throw err
-    console.log('Папка создана')
+    console.log('Файл был создан')
 })
