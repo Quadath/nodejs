a331b981115d8bd92dbdf2eee30f2365e9f0944b
Quadatz <quadath@gmail.com>
2023-01-16 17:33:17 +0300

    os module

---
+++ refs/os_ref.js
@@ -0,0 +1,16 @@
+const os = require('os')
+
+console.log(os.platform());
+//win32
+console.log(os.arch());
+//x64
+console.log(os.cpus());
+//CPU INFO
+console.log(os.freemem());
+//7974526976 FREE MEMORY
+console.log(os.totalmem());
+//14978269184 TOTAL MEMORY
+console.log(os.homedir());
+//C:\Users\Роман
+console.log(os.uptime());
+//UPTIME
