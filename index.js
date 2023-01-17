8a5040c096fef3f76777301336830c0ac98aa746
Quadatz <quadath@gmail.com>
2023-01-17 13:58:42 +0300

    http server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const user = require('./user')
+const http = require('http')
 
-console.log(user)
+let a = 0;
+let b = 10;
+
+const server = http.createServer((req, res) => {
+    // console.log(req.statusCode)
+    a++;
+    // res.write("Hello from NODEJS")
+    res.write("<h1>Hello from NODEJS</h1>")
+
+    res.end(`<div>${a}:${b}</div>`)
+})
+
+server.listen(3000, () => {
+    console.log('Сервер запущен')
+})
